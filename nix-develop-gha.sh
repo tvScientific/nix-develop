dc10a55549ab8b0a1ef96bfe3d12ec5c9293808b
Ellie Ford <eford@pinterest.com>
2026-05-27 21:07:02 -0700

    okay, skip over empty variable names

--- nix-develop-gha.sh
+++ nix-develop-gha.sh
@@ -23,6 +23,10 @@ while IFS='=' read -r -d '' n v || exit "$n"; do
 		envOutput=1
 		if [ "$v" ]; then
 			# There was one or more equals signs in the hook output
+			if [[ "$n" =~ ^[[:space:]]*$ ]]; then
+				# We got a variable that was just spaces, which is unlikely to be intentional, so skip it.
+				continue
+			fi
 			printf "%s=%s" "$n" "$v"
 		else
 			printf "%s" "$n"
